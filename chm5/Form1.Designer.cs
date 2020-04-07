6b724f0af6360dce98c81b072818068123bb5b21
PinusThunbergii <aleksey.maksymyshyn@gmail.com>
2020-04-07 12:09:18 +0300

    trte

--- chm5/Form1.Designer.cs
+++ chm5/Form1.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.x0TextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.epsilonTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kTextBox = new System.Windows.Forms.TextBox();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Location = new System.Drawing.Point(75, 66);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(100, 20);
+            this.aTextBox.TabIndex = 0;
+            this.aTextBox.Text = "0.8";
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Location = new System.Drawing.Point(75, 92);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bTextBox.TabIndex = 1;
+            this.bTextBox.Text = "1.2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "b";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(225, 95);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.onClick_CalculateButton);
+            // 
+            // x0TextBox
+            // 
+            this.x0TextBox.Location = new System.Drawing.Point(75, 118);
+            this.x0TextBox.Name = "x0TextBox";
+            this.x0TextBox.Size = new System.Drawing.Size(100, 20);
+            this.x0TextBox.TabIndex = 5;
+            this.x0TextBox.Text = "0.85";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "e";
+            // 
+            // epsilonTextBox
+            // 
+            this.epsilonTextBox.Location = new System.Drawing.Point(75, 144);
+            this.epsilonTextBox.Name = "epsilonTextBox";
+            this.epsilonTextBox.Size = new System.Drawing.Size(100, 20);
+            this.epsilonTextBox.TabIndex = 7;
+            this.epsilonTextBox.Text = "0.00002";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(361, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "k";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(361, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "x";
+            // 
+            // kTextBox
+            // 
+            this.kTextBox.Location = new System.Drawing.Point(386, 114);
+            this.kTextBox.Name = "kTextBox";
+            this.kTextBox.Size = new System.Drawing.Size(100, 20);
+            this.kTextBox.TabIndex = 10;
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(386, 88);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xTextBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(594, 224);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.kTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.epsilonTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.x0TextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.aTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox x0TextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox epsilonTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox kTextBox;
+        private System.Windows.Forms.TextBox xTextBox;
     }
 }
 
--- chm5/Form1.cs
+++ chm5/Form1.cs
@@ -12,9 +12,65 @@ namespace chm5
 {
     public partial class Form1 : Form
     {
+        private double a;
+        private double b;
+        private double epsilon;
+        private double x0;
+        private double x;
+        private int k;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        delegate double Function(double x);
+
+        //[0.8; 1.2] x0=0.85 epsilon = 0.00002 root = 0.978
+        double phi(double x)
+        {
+            return (Math.Log(x * 6.0976d) + 2.0d) / 3.872d;
+            //return (Math.Log(x * 6.0976d) + 1.0d) / 6.872d;
+        }
+
+        void calculate(double a, double b, double epsilon, double x0, Function phi, out double x, out int k)
+        {
+   
+            k = 0;
+            do
+            {
+                x = x0;
+                x0 = phi(x0);
+                k++;
+                Console.WriteLine($"x = {x} x0 = {x0} k = {k}");
+            }
+            while (Math.Abs(x - x0) > epsilon);
+        }
+
+        void onClick_CalculateButton(object sender, EventArgs eventArgs)
+        {
+            try
+            {
+                if(!(Double.TryParse(aTextBox.Text, out a) &&
+                    Double.TryParse(bTextBox.Text, out b) &&
+                    Double.TryParse(epsilonTextBox.Text, out epsilon) &&
+                    Double.TryParse(x0TextBox.Text, out x0)))
+                {
+                    throw new Exception("Input error");
+                }
+                if(x0 < a || x0 > b)
+                {
+                    throw new Exception($"x0 isn`t in range [{a};{b}]");
+                }
+                calculate(a, b, epsilon, x0, phi, out x, out k);
+
+                xTextBox.Text = $"{x}";
+                kTextBox.Text = $"{k}";
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
